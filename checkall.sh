6fb1fde7cd00a982df610274bac319e740d86438
seungjun <stormjun55@gmail.com>
2021-04-09 15:33:21 +0900

    prevent infinite loop for c/c++ code and root code

--- checkall.sh
+++ checkall.sh
@@ -2,15 +2,15 @@
 
 
 echo "Start fucking Grading"
-
 #g++ -o filename filename.cpp
 #./filename
-for i in /home/seungjun/cal_HW1/test*.cc ; do
-name=$i
-output=${name/.cc/.out}
-g++ -o output $i
+for i in /Users/noise/Documents/TA/cal_basic_kim_2021_1/2_hw/home*.cc ; do
+#name=$i
+#output=${name/.cc/.out}
+#g++ -o output $i
+output=${name:52}
 echo $output
-./output
+timeout 30  root -q $i+
 echo  "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
 echo  "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
 echo  "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
@@ -19,11 +19,11 @@ echo  "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
 done
 
 
-for i in /home/seungjun/cal_HW1/test*.cc ; do
-
-name=$i
-rm -rf ${name:1:-3}
-done
+#for i in /home/seungjun/cal_HW1/test*.cc ; do
+#
+#name=$i
+#rm -rf ${name:1:-3}
+#done
 
 
 
