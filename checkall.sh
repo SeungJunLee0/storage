918d6c853b05a070b41d6b515036ce0e0a7699e6
seungjun <stormjun55@gmail.com>
2022-04-08 20:29:32 +0900

    UpGrade grading tools

--- checkall.sh
+++ checkall.sh
@@ -1,29 +1,14 @@
 #!/bin/bash
 
-
 echo "Start fucking Grading"
-#g++ -o filename filename.cpp
-#./filename
-for i in /Users/noise/Documents/TA/cal_basic_kim_2021_1/2_hw/home*.cc ; do
-#name=$i
-#output=${name/.cc/.out}
-#g++ -o output $i
-output=${name:52}
-echo $output
-timeout 30  root -q $i+
-echo  "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
-echo  "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
-echo  "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
-echo  "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
-echo  "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
+for i in *.cc; do
+name=$i
+echo "                "$name
+output=${name/.cc/}
+g++ $name -o $output
+./$output
+read id
 done
 
-
-#for i in /home/seungjun/cal_HW1/test*.cc ; do
-#
-#name=$i
-#rm -rf ${name:1:-3}
-#done
-
-
-
+###########################
+#for i in *.cc;do name=$i;output=${name/.cc/};rm -rf $output;done
